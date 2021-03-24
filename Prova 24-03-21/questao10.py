41afd565074458099a294ce3cf3d9db601c0a695
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-24 20:08:17 -0300

    prova

--- Prova 24-03-21/questao10.py
+++ Prova 24-03-21/questao10.py
@@ -3,3 +3,16 @@ faça o que se pede a seguir:
 ● Uma lista com os números pares
 ● uma lista com os números múltiplos de 5'''
 
+lista = []
+for i in range(1, 101):
+    lista.append(i)
+print('Lista:')
+print(lista)
+
+lista_02 = [numero for numero in range(1, 101) if numero % 2 == 0]
+print('Números pares:')
+print(lista_02)
+
+lista_03 = [numero for numero in range(1, 101) if numero % 5 == 0]
+print('Multiplos de 5:')
+print(lista_03)
